f9240aa93c0a8ebac38e02dee699ea6352bc585a
AngeelDL <angeel.dl1@gmail.com>
2023-08-19 14:14:06 -0600

    Practica 1

---
+++ script.js
@@ -0,0 +1,14 @@
+var miPrimerVariable= 'Hola, Mundo';
+
+var campoTexto= document.getElementById('mitexto1');
+
+
+function mostrarAlerta() {
+    alert(campoTexto.value);
+}
+
+function limpiarTexto(){
+    campoTexto.value=null;
+   
+}
+
