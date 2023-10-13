14e22b62c38223f07aee33da03f3fc370eb65c35
GloriaOSTOS <mygeos23@gmail.com>
2023-10-12 18:45:41 -0600

    Practica8: Action Sheet y Sliding Options

--- miApp-2086127/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2086127/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
-  styleUrls: ['./lista-alumnos.component.scss'],
+  styleUrls: ['./lista-alumnos.component.scss']
 })
-export class ListaAlumnosComponent  implements OnInit {
 
-  constructor() { }
+export class ListaAlumnosComponent implements OnInit {  
+  
+  constructor(private actionSheetCtrl: ActionSheetController) { }
+  
+  ngOnInit(): void {  
+  }  
+  nombre: string = "Hola";
+  
+  alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Barbara", "Georgina", "Kevin"];
 
-  ngOnInit() {}
+  
+  async presentActionSheet(alumno: string, event: Event) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: '¿Está seguro de que desea eliminar este alumno?',
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+      
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
 
-  alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+    await actionSheet.present();
+  }
 
 }
