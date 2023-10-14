ee51bebd674071078b7148e0b823de63324f58c0
GloriaOSTOS <mygeos23@gmail.com>
2023-10-14 14:00:53 -0600

    Práctica 9: Servicios en Angular

--- miApp-2086127/src/app/home/home.module.ts
+++ miApp-2086127/src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { ListaAlumnosComponent } from '../lista-alumnos/lista-alumnos.component';
+import { ProductosComponent } from '../productos/productos.component';
 
 
 @NgModule({
@@ -15,6 +16,6 @@ import { ListaAlumnosComponent } from '../lista-alumnos/lista-alumnos.component'
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage, ListaAlumnosComponent]
+  declarations: [HomePage, ListaAlumnosComponent,ProductosComponent]
 })
 export class HomePageModule {}
