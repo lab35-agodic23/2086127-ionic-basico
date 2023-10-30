1139d5ed5052f671721756140fc7e7ad4f04f70f
GloriaOSTOS <mygeos23@gmail.com>
2023-10-30 14:48:04 -0600

    Practica 11

--- miApp-2086127/src/app/app-routing.module.ts
+++ miApp-2086127/src/app/app-routing.module.ts
@@ -1,25 +1,34 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
 import { ProductosComponent } from './productos/productos.component';
 import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
+import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+
+
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  
   {
-    path:'productos',
-    component : ProductosComponent
+    path: 'produtos',
+    component: ProductosComponent,
   },
   {
-    path: 'producto/:id',
-    component : ProductoDetalleComponent,
+    path: 'alumnos',
+    component: ListaAlumnosComponent,
   },
   {
     path: '',
-    redirectTo: '/productos',
+    redirectTo: '/alumnos',
     pathMatch: 'full'
+  },
+  {
+    path: 'producto/:id',
+    component: ProductoDetalleComponent,
   },
 ];
 
--- miApp-2086127/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2086127/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,46 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { Alumno } from './alumno.model';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss']
 })
+export class ListaAlumnosComponent  {
+  nuevoAlumno: Alumno;
+  alumnos: Alumno[] = [];
 
-export class ListaAlumnosComponent implements OnInit {  
-  
-  constructor(private actionSheetCtrl: ActionSheetController) { }
-  
-  ngOnInit(): void {  
-  }  
-  nombre: string = "Hola";
-  
-  alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Barbara", "Georgina", "Kevin"];
-
-  
-  async presentActionSheet(alumno: string, event: Event) {
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: '¿Está seguro de que desea eliminar este alumno?',
-      buttons: [
-        {
-          text: 'Eliminar',
-          role: 'destructive',
-          data: {
-            action: 'delete',
-          },
-        },
-      
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          data: {
-            action: 'cancel',
-          },
-        },
-      ],
-    });
-
-    await actionSheet.present();
+  constructor() {
+    this.nuevoAlumno = { nombre: '', presente: false };
   }
 
+  agregarAlumno() {
+    if (this.nuevoAlumno.nombre) {
+      this.alumnos.push(this.nuevoAlumno);
+      this.nuevoAlumno = { nombre: '', presente: false };
+    }
+  }
+
+  ngOnInit() {}
+
+  alumno : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 }
