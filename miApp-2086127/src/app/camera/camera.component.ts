e13f88c76aa67a5425fa83484b21c8e34802f045
GloriaOSTOS <mygeos23@gmail.com>
2023-11-08 15:36:50 -0600

    Practica 12: Parte 2

--- miApp-2086127/src/app/camera/camera.component.ts
+++ miApp-2086127/src/app/camera/camera.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FotoServiceService } from '../foto-service.service';
-
+import { Foto } from './foto.model';
 
 
 @Component({
@@ -8,17 +8,21 @@ import { FotoServiceService } from '../foto-service.service';
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.scss'],
 })
-export class CamaraComponent  implements OnInit {
+export class CamaraComponent implements OnInit {
+
+  public fotos: Foto[] = []; 
 
   constructor(public photoService: FotoServiceService) { }
 
-  ngOnInit() {}
-
+  ngOnInit() {
+   
+    this.fotos = this.photoService.fotos;
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
-
 }
+
 
 
--- miApp-2086127/src/app/foto-service.service.ts
+++ miApp-2086127/src/app/foto-service.service.ts
@@ -2,21 +2,38 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { Foto } from './camera/foto.model';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FotoServiceService {
 
+  public fotos: Foto[] = [];
+
   constructor() { }
 
   public async addNewToGallery() {
-    // Take a photo
-    const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
-    });
+    try {
+      
+      const capturedPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100
+      });
+
+      const newPhoto: Foto = {
+      filepath: capturedPhoto.path,
+        webViewPath: capturedPhoto.webPath
+      };
+
+      this.fotos.unshift(newPhoto);
+    } catch (error) {
+      console.error('Error al capturar la foto:', error);
+    }
   }
 }
+
 
